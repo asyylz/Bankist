e96ca516b6c3d5a61bb7a2ff3cc4026c34c2f8af
asyylz <49663592+asyylz@users.noreply.github.com>
2024-01-26 00:56:19 +0000

    Tabbed Component built

--- script.js
+++ script.js
@@ -1,52 +1,55 @@
-"use strict";
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".btn--close-modal");
-const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 /* -------------------- Modal window -------------------- */
 const openModal = function (e) {
   e.preventDefault();
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
 
 /* ------------- 'Learn more' btn scrolling ------------- */
-btnScrollTo.addEventListener("click", function (e) {
+btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
   console.log(e.target.getBoundingClientRect());
-  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
   console.log(
-    "height/width viewport",
+    'height/width viewport',
     document.documentElement.clientHeight,
     document.documentElement.clientWidth
   );
-  section1.scrollIntoView({ behavior: "smooth" });
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
 
 /* ------------------- Cookie message ------------------- */
-const header = document.querySelector(".header");
-const message = document.createElement("div");
-message.classList.add("cookie-message");
+const header = document.querySelector('.header');
+const message = document.createElement('div');
+message.classList.add('cookie-message');
 message.innerHTML =
   'We use cookied for improved functionality  and analytics.<button class="btn btn--close-cookie">Got it!</button>';
 
@@ -54,22 +57,41 @@ header.append(message);
 
 /* ------------ Close cookie message element ------------ */
 document
-  .querySelector(".btn--close-cookie")
-  .addEventListener("click", function () {
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
     message.remove();
   });
 
 /* ------------------------ Style ----------------------- */
-message.style.backgroundColor = "#37383d";
-message.style.width = "120%";
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
 message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
 /* ------------------- Page Navigation ------------------ */
-document.querySelector(".nav__links").addEventListener("click", function (e) {
+document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
-  if (e.target.classList.contains("nav__link")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
+});
+
+/* ------------- Building a Tabbed Component ------------ */
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
